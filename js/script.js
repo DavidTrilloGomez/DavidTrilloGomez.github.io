0b14a0a3153552a3d942c84ccc024b3e7892d13b
David Trillo <david.trillo@escuelaestech.es>
2025-01-22 20:54:55 +0100

    version 1.4 fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const form = document.querySelector('#contactoForm')
 
+const serviceId = "service_oazyo6k"
+const templateId = "template_fgbcfmb"
+const apikey = "ao9J5WhZ0Hdafu2y9"
+
 form.addEventListener('submit', handleSubmit)
+
 
 
 function handleSubmit(event){
     event.preventDefault()
+    emailjs.init(serviceId)
+
+    emailjs.sendForm(serviceId, templateId, form, apikey).then(result => FileSystemWritableFileStream.fire('Su mensaje se ha enviado con éxito.'))
 }
+
